8628d57c349dfe779b8a0f53ed2f7302c2990669
Raghavistical <raghavistical@gmail.com>
2026-07-15 21:15:09 +0530

    Add files via upload

---
+++ GEN_AI career/Day_01/basics.py
@@ -0,0 +1,14 @@
+name = input("What is your name?: ")
+age = input ("Enter your age: ")
+bike = input ("What is your favourite bike?: ")
+salary = input ("what is your dream salary?: ")
+
+print("--------------------PROFILE--------------------")
+print()
+print("Your name              :", name)
+print("Your age is            :", age)
+print("Your favourite bike is :", bike)
+print("Your dream salary is   :", salary)
+print()
+print("-----------------------------------------------")
+
